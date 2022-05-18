15820a26523a9524abca9c45728a85733787b497
RajiSelvakumar <rajeswariselvakumar06@gmail.com>
2022-05-18 09:08:58 +0530

    test

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::get('/login',[CustomAuthController::class,'login']);
 Route::get('/',[CustomAuthController::class,'registration']);
 Route::post('/register-user',[CustomAuthController::class,'registerUser'])->name('register-user');
 Route::post('/login-user',[CustomAuthController::class,'loginUser'])->name('login-user');
-Route::get('/dashboard',[CustomAuthController::class,'dashboard'])->middleware('isLoggedIn');
+Route::get('/dashboard',[CustomAuthController::class,'dashboard']);
 Route::get('/logout', [CustomAuthController::class, 'logout']);
 Route::get('/edit', [CustomAuthController::class, 'edit']);
 Route::put('/update-user',[CustomAuthController::class, 'updateUser'])->name('update-user');
